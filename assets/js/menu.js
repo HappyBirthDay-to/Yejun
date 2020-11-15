47d04c3f94f5f4582d37857e5fc0c9af23373796
HappyBirthDay-to <mire256@naver.com>
2020-11-15 16:31:44 +0900

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -10,33 +10,32 @@
  */
 (function() {
 
-	var bodyEl = document.body,
-		content = document.querySelector( '.content-wrap' ),
-		openbtn = document.getElementById( 'open-button' ),
-		closebtn = document.getElementById( 'close-button' ),
-		isOpen = false;
+    var bodyEl = document.body,
+        content = document.querySelector('.content-wrap'),
+        openbtn = document.getElementById('open-button'),
+        closebtn = document.getElementById('close-button'),
+        isOpen = false;
 
-	function init() {
-		initEvents();
-	}
+    function init() {
+        initEvents();
+    }
 
-	function initEvents() {
-		openbtn.addEventListener( 'click', toggleMenu );
-		if( closebtn ) {
-			closebtn.addEventListener( 'click', toggleMenu );
-		}		
-	}
+    function initEvents() {
+        openbtn.addEventListener('click', toggleMenu);
+        if (closebtn) {
+            closebtn.addEventListener('click', toggleMenu);
+        }
+    }
 
-	function toggleMenu() {
-		if( isOpen ) {
-			classie.remove( bodyEl, 'show-menu' );
-		}
-		else {
-			classie.add( bodyEl, 'show-menu' );
-		}
-		isOpen = !isOpen;
-	}
+    function toggleMenu() {
+        if (isOpen) {
+            classie.remove(bodyEl, 'show-menu');
+        } else {
+            classie.add(bodyEl, 'show-menu');
+        }
+        isOpen = !isOpen;
+    }
 
-	init();
+    init();
 
 })();
